bb6780ca406de56ba6efe99fffa1513d828d3636
Teffandi <13221084@std.stei.itb.ac.id>
2024-07-10 16:33:27 +0800

    rehaul core structure

--- RTL/Radix_2/radix_2.v
+++ RTL/Radix_2/radix_2.v
@@ -2,8 +2,7 @@
 module radix_2 #(`include "radix_2_parameter.vh")(
    input [width-1:0] input_1, 
    input [width-1:0] input_2,
-   input [width-1:0] input_twiddle_1,
-   input [width-1:0] input_twiddle_2,
+   input [width-1:0] input_twiddle,
    input  select, //0 for NTT and 1 for INTT
    output [width-1:0] output_1,
    output [width-1:0] output_2
@@ -21,23 +20,18 @@ adder_2 add_1(.Operand_1(temp_input_adder_1),.Operand_2(temp_input_adder_2),.mod
 
 
 //=========================================================== multiplier structure begin
-wire [width-1:0] temp_input_mult_1;
-wire [width-1:0] temp_input_mult_2;
-wire [width-1:0] temp_output_mult_1;
-wire [width-1:0] temp_output_mult_2;
+wire [width-1:0] temp_input_mult_0;
+wire [width-1:0] temp_output_mult_0;
 
-mult mult_0(.Operand_1(temp_input_mult_1),.Operand_2(input_twiddle_1),.output_mult(temp_output_mult_1));
-mult mult_1(.Operand_1(temp_input_mult_2),.Operand_2(input_twiddle_2),.output_mult(temp_output_mult_2));
+mult mult_0 (.Operand_1(temp_input_mult_0),.Operand_2(input_twiddle),.output_mult(temp_output_mult_0));
 //=========================================================== multiplier structure end
 
 
 //=========================================================== core structure begin
-mux_2_1 mux_0(.input_1(input_1),.input_2(temp_output_adder_1),.select(select),.out(temp_input_mult_1));
-mux_2_1 mux_1(.input_1(temp_output_mult_1),.input_2(input_1),.select(select),.out(temp_input_adder_1));
-mux_2_1 mux_2(.input_1(temp_output_mult_2),.input_2(input_2),.select(select),.out(temp_input_adder_2));
-mux_2_1 mux_3(.input_1(input_2),.input_2(temp_output_adder_2),.select(select),.out(temp_input_mult_2));
-mux_2_1 mux_4(.input_1(temp_output_adder_1),.input_2(temp_output_mult_1),.select(select),.out(output_1));
-mux_2_1 mux_5(.input_1(temp_output_adder_2),.input_2(temp_output_mult_2),.select(select),.out(output_2));
+mux_2_1 mux_0(.input_1(temp_output_mult_0),.input_2(input_2),.select(select),.out(temp_input_adder_2));
+mux_2_1 mux_1(.input_1(input_2),.input_2(temp_output_adder_2),.select(select),.out(temp_input_mult_0));
+mux_2_1 mux_2(.input_1(temp_output_adder_2),.input_2(temp_output_mult_0),.select(select),.out(output_2));
+assign output_1 = temp_output_adder_1;
 //=========================================================== core structure end
 
 endmodule
