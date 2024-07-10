7eebf48923b0bd3ac9e71a6ca0645c941641c73b
Teffandi <13221084@std.stei.itb.ac.id>
2024-07-10 14:30:32 +0800

    fixed illegal net refernce

--- RTL/Radix_2/adder_2_input.v
+++ RTL/Radix_2/adder_2_input.v
@@ -4,11 +4,11 @@ module adder_2 #(`include "radix_2_parameter.vh")(
 input [width-1:0] Operand_1,
 input [width-1:0] Operand_2,
 input mode, // 0 to add, 1 to subtract
-output reg [width-1:0] adder_output
+output [width-1:0] adder_output
 );
 
-wire mux_0_to_add [width-1:0];
+wire [width-1:0] mux_0_to_add ;
 mux_2_1 mux_0 (.input_1(Operand_2),.input_2(~(Operand_2)),.select(mode),.out(mux_0_to_add));
-adder_output = Operand_1 + mux_0_to_add + mode;
+assign adder_output = Operand_1 + mux_0_to_add + mode;
 
 endmodule
--- RTL/Radix_2/radix_2.v
+++ RTL/Radix_2/radix_2.v
@@ -10,10 +10,10 @@ module radix_2 #(`include "radix_2_parameter.vh")(
 );
 
 //=========================================================== butterfly structure begin
-wire temp_input_adder_1[width-1:0];
-wire temp_input_adder_2[width-1:0];
-wire temp_output_adder_1[width-1:0];
-wire temp_output_adder_2[width-1:0];
+wire [width-1:0] temp_input_adder_1;
+wire [width-1:0] temp_input_adder_2;
+wire [width-1:0] temp_output_adder_1;
+wire [width-1:0] temp_output_adder_2;
 
 adder_2 add_0(.Operand_1(temp_input_adder_1),.Operand_2(temp_input_adder_2),.mode(1'b0),.adder_output(temp_output_adder_1)); //add
 adder_2 add_1(.Operand_1(temp_input_adder_1),.Operand_2(temp_input_adder_2),.mode(1'b1),.adder_output(temp_output_adder_2));//subtract
@@ -21,10 +21,10 @@ adder_2 add_1(.Operand_1(temp_input_adder_1),.Operand_2(temp_input_adder_2),.mod
 
 
 //=========================================================== multiplier structure begin
-wire temp_input_mult_1[width-1:0];
-wire temp_input_mult_2[width-1:0];
-wire temp_output_mult_1[width-1:0];
-wire temp_output_mult_2[width-1:0];
+wire [width-1:0] temp_input_mult_1;
+wire [width-1:0] temp_input_mult_2;
+wire [width-1:0] temp_output_mult_1;
+wire [width-1:0] temp_output_mult_2;
 
 mult mult_0(.Operand_1(temp_input_mult_1),.Operand_2(input_twiddle_1),.output_mult(temp_output_mult_1));
 mult mult_1(.Operand_1(temp_input_mult_2),.Operand_2(input_twiddle_2),.output_mult(temp_output_mult_2));
@@ -32,12 +32,12 @@ mult mult_1(.Operand_1(temp_input_mult_2),.Operand_2(input_twiddle_2),.output_mu
 
 
 //=========================================================== core structure begin
-mux_2_1 mux_0(.input_1(input_1),.input_2(temp_output_adder_1),.select(select),.output(temp_input_mult_1));
-mux_2_1 mux_1(.input_1(temp_output_mult_1),.input_2(input_1),.select(select),.output(temp_input_adder_1));
-mux_2_1 mux_2(.input_1(temp_output_mult_2),.input_2(input_2),.select(select),.output(temp_input_adder_2));
-mux_2_1 mux_3(.input_1(input_2),.input_2(temp_output_adder_2),.select(select),.output(temp_input_mult_2));
-mux_2_1 mux_4(.input_1(temp_output_adder_1),.input_2(temp_output_mult_1),.select(select),.output(output_1));
-mux_2_1 mux_5(.input_1(temp_output_adder_2),.input_2(temp_output_mult_2),.select(select),.output(output_2));
+mux_2_1 mux_0(.input_1(input_1),.input_2(temp_output_adder_1),.select(select),.out(temp_input_mult_1));
+mux_2_1 mux_1(.input_1(temp_output_mult_1),.input_2(input_1),.select(select),.out(temp_input_adder_1));
+mux_2_1 mux_2(.input_1(temp_output_mult_2),.input_2(input_2),.select(select),.out(temp_input_adder_2));
+mux_2_1 mux_3(.input_1(input_2),.input_2(temp_output_adder_2),.select(select),.out(temp_input_mult_2));
+mux_2_1 mux_4(.input_1(temp_output_adder_1),.input_2(temp_output_mult_1),.select(select),.out(output_1));
+mux_2_1 mux_5(.input_1(temp_output_adder_2),.input_2(temp_output_mult_2),.select(select),.out(output_2));
 //=========================================================== core structure end
 
 endmodule
