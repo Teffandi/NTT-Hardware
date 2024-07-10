24698e759bcf00530e420f9b7b275b50dd11f675
Teffandi <13221084@std.stei.itb.ac.id>
2024-07-10 14:37:28 +0800

    fix some issues

--- RTL/Radix_2/mult.v
+++ RTL/Radix_2/mult.v
@@ -2,12 +2,12 @@
 module mult #(`include "radix_2_parameter.vh")(
 input [width-1:0] Operand_1,
 input [width-1:0] Operand_2,
-output[width-1:0] mult_output
+output[width-1:0] output_mult
 );
 
 wire [2*width-1:0]temp;
 assign temp = Operand_1[width-1:0]*Operand_2[width-1:0];
-assign mult_output[15] = Operand_1[width-1]^Operand_2[width-1];
-assign mult_output = temp[2*width-9:width-8];
+assign output_mult[width-1] = Operand_1[width-1]^Operand_2[width-1];
+assign output_mult[width-2:0] = temp[2*width-9:width-8];
 
 endmodule
