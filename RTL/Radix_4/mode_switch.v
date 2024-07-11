862fba3ca2643568ef3c18a36876cc8450dec8c1
Teffandi <13221084@std.stei.itb.ac.id>
2024-07-11 13:29:07 +0800

    fix unconnected output

--- RTL/Radix_4/mode_switch.v
+++ RTL/Radix_4/mode_switch.v
@@ -15,7 +15,7 @@ wire [width-1:0] temp_output_mux_1;
 
 mux_2_1 mux_0(.input_1(temp_output_mult_0),.input_2(input_switch),.select(select),.out(butterfly_input));
 mux_2_1 mux_1(.input_1(input_switch),.input_2(butterfly_output),.select(select),.out(temp_output_mux_1));
-mux_2_1 mux_2(.input_1(butterfly_output),.input_2(temp_output_mult_0),.select(select),.out());
+mux_2_1 mux_2(.input_1(butterfly_output),.input_2(temp_output_mult_0),.select(select),.out(output_switch));
 mult mult_0 (.Operand_1(temp_output_mux_1),.Operand_2(twiddle_factor),.output_mult(temp_output_mult_0));
 
 endmodule
