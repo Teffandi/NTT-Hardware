dcbdec296dc0a1707c4137d9c44c66e04e70df83
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2024-07-12 22:41:04 +0700

    Fixed Matrix Generator with consideration of periodicity and symmmetricity

--- RTL/matrix_generator.py
+++ RTL/matrix_generator.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-N = int(input("enter total input point :")) #input size
-prime = int(input("enter prime number used :")) #input prime number
+
+# N = int(input("enter total input point :")) #input size
+# prime = int(input("enter prime number used :")) #input prime number
+
+N = 4
+prime = 7681
 
 possible_primitive=[] 
 verified_primitive=[] 
@@ -26,7 +30,9 @@ print(possible_primitive)
 primitive_verification(possible_primitive,prime,N)
 print(verified_primitive)
 
-selected_primitive_positive = int(input("enter primitive number selected :")) #input selected primitive generated from list (PWC primitive)
+# selected_primitive_positive = int(input("enter primitive number selected :")) #input selected primitive generated from list (PWC primitive)
+
+selected_primitive_positive = 3383
 
 #searching for NWC primitive
 possible_primitive_negative=[] 
@@ -39,14 +45,36 @@ def primitive_search_negative(prime,selected_primitive_positive) :
 
 primitive_search_negative(prime,selected_primitive_positive)
 print(possible_primitive_negative)
-selected_primitive_negative = int(input("enter negative primitive number selected :")) #input selected primitive generated from list (PWC primitive)
+# selected_primitive_negative = int(input("enter negative primitive number selected :")) #input selected primitive generated from list (PWC primitive)
+selected_primitive_negative = 1925
+
+check_result = []
+check_result.append(pow(selected_primitive_negative,1) % prime)
+check_result.append(pow(selected_primitive_negative,0) % prime)
+check_result.append(pow(selected_primitive_negative,2) % prime)
+check_result.append(pow(selected_primitive_negative,3) % prime)
+
+print(f'Check Result:')
+print(check_result)
 
 #generating matrix 
-matrix = np.zeros((int(N),int(N)))
+# matrix = np.zeros((int(N),int(N)))
+matrix = [[1]*N for i in range(0,N)]
 #generating coeffient
 for row in range(0,N):
     for col in range(0,N):
-        matrix[row][col]= pow(selected_primitive_negative,2*(row*col)+col)%prime
+        power = (2*(row*col)+col) % (2*N)       # Periodicity
+
+        if (power//N) % 2 == 1:                 # Symmetricity
+            power = power % N
+            matrix[row][col] *= -1
+
+        matrix[row][col] *= pow(selected_primitive_negative,power)%prime
+
         col=col+1
     row=row+1
+
+print(f'Result:')
+for row in range(0,N):
+    print(matrix[row])
 
