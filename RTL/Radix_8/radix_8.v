86750260c1784b87857944aba87837a7dc199f10
Teffandi <13221084@std.stei.itb.ac.id>
2024-07-15 15:02:32 +0800

    radix 8 first prototype (untested)

--- RTL/Radix_8/radix_8.v
+++ RTL/Radix_8/radix_8.v
@@ -47,10 +47,14 @@ wire [width-1:0] temp_output_adder_6;
 wire [width-1:0] temp_output_adder_7;
 wire [width-1:0] temp_output_adder_8;
 
-assign temp_output_adder_1 = temp_input_adder_1 + temp_input_adder_2 + temp_input_adder_3 + temp_input_adder_4;
-assign temp_output_adder_2 = temp_input_adder_1 - temp_input_adder_2 - temp_input_adder_3 + temp_input_adder_4;
-assign temp_output_adder_3 = temp_input_adder_1 - temp_input_adder_2 + temp_input_adder_3 - temp_input_adder_4;
-assign temp_output_adder_4 = temp_input_adder_1 + temp_input_adder_2 - temp_input_adder_3 - temp_input_adder_4;
+assign temp_output_adder_1 = temp_input_adder_1 + w2*temp_input_adder_2 + w3*temp_input_adder_3 + w4*temp_input_adder_4 + w5*temp_input_adder_5 + w6*temp_input_adder_6 + w7*temp_input_adder_7 + w8*temp_input_adder_8;
+assign temp_output_adder_2 = temp_input_adder_1 + w4*temp_input_adder_2 + w7*temp_input_adder_3 - w2*temp_input_adder_4 - w5*temp_input_adder_5 - w8*temp_input_adder_6 + w3*temp_input_adder_7 + w6*temp_input_adder_8;
+assign temp_output_adder_3 = temp_input_adder_1 + w6*temp_input_adder_2 - w3*temp_input_adder_3 - w8*temp_input_adder_4 + w5*temp_input_adder_5 - w2*temp_input_adder_6 - w7*temp_input_adder_7 + w4*temp_input_adder_8;
+assign temp_output_adder_4 = temp_input_adder_1 + w8*temp_input_adder_2 - w7*temp_input_adder_3 + w6*temp_input_adder_4 - w5*temp_input_adder_5 + w4*temp_input_adder_6 - w3*temp_input_adder_7 + w2*temp_input_adder_8;
+assign temp_output_adder_5 = temp_input_adder_1 - w2*temp_input_adder_2 + w3*temp_input_adder_3 - w4*temp_input_adder_4 + w5*temp_input_adder_5 - w6*temp_input_adder_6 + w7*temp_input_adder_7 - w8*temp_input_adder_8;
+assign temp_output_adder_6 = temp_input_adder_1 - w4*temp_input_adder_2 + w7*temp_input_adder_3 + w2*temp_input_adder_4 - w5*temp_input_adder_5 + w8*temp_input_adder_6 + w3*temp_input_adder_7 - w6*temp_input_adder_8;
+assign temp_output_adder_7 = temp_input_adder_1 - w6*temp_input_adder_2 - w3*temp_input_adder_3 + w8*temp_input_adder_4 + w5*temp_input_adder_5 + w2*temp_input_adder_6 - w7*temp_input_adder_7 - w4*temp_input_adder_8;
+assign temp_output_adder_8 = temp_input_adder_1 - w8*temp_input_adder_2 - w7*temp_input_adder_3 - w6*temp_input_adder_4 - w5*temp_input_adder_5 - w4*temp_input_adder_6 - w3*temp_input_adder_7 - w2*temp_input_adder_8;
 //=========================================================== butterfly structure end
 
 
@@ -61,6 +65,11 @@ assign output_1 = temp_output_adder_1;
 mode_switch mode_switch_0 (.input_switch(input_2),.twiddle_factor(input_twiddle_2),.butterfly_output(temp_output_adder_2),.select(select),.butterfly_input(temp_input_adder_2),.output_switch(output_2));
 mode_switch mode_switch_1 (.input_switch(input_3),.twiddle_factor(input_twiddle_3),.butterfly_output(temp_output_adder_3),.select(select),.butterfly_input(temp_input_adder_3),.output_switch(output_3));
 mode_switch mode_switch_2 (.input_switch(input_4),.twiddle_factor(input_twiddle_4),.butterfly_output(temp_output_adder_4),.select(select),.butterfly_input(temp_input_adder_4),.output_switch(output_4));
+mode_switch mode_switch_3 (.input_switch(input_5),.twiddle_factor(input_twiddle_5),.butterfly_output(temp_output_adder_5),.select(select),.butterfly_input(temp_input_adder_5),.output_switch(output_5));
+mode_switch mode_switch_4 (.input_switch(input_6),.twiddle_factor(input_twiddle_6),.butterfly_output(temp_output_adder_6),.select(select),.butterfly_input(temp_input_adder_6),.output_switch(output_6));
+mode_switch mode_switch_5 (.input_switch(input_7),.twiddle_factor(input_twiddle_7),.butterfly_output(temp_output_adder_7),.select(select),.butterfly_input(temp_input_adder_7),.output_switch(output_7));
+mode_switch mode_switch_6 (.input_switch(input_8),.twiddle_factor(input_twiddle_8),.butterfly_output(temp_output_adder_8),.select(select),.butterfly_input(temp_input_adder_8),.output_switch(output_8));
+
 //=========================================================== core structure end
 
 endmodule
--- RTL/dummy.v
+++ RTL/dummy.v
@@ -1,1 +1,17 @@
 
+[[ w1  w2  w3  w4  w5  w6
+   w7  w8]
+ [ w1  w4  w7 -w2 -w5 -w8
+   w3  w6]
+ [ w1  w6 -w3 -w8  w5 -w2
+  -w7  w4]
+ [ w1  w8 -w7  w6 -w5  w4
+  -w3  w2]
+ [ w1 -w2  w3 -w4  w5 -w6
+   w7 -w8]
+ [ w1 -w4  w7  w2 -w5  w8
+   w3 -w6]
+ [ w1 -w6 -w3  w8  w5  w2
+  -w7 -w4]
+ [ w1 -w8 -w7 -w6 -w5 -w4
+  -w3 -w2]]
