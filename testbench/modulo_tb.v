6331fcb596d484782e8f7c014ce726a64b45fb34
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2024-07-22 09:30:07 +0700

    Tested modulo implementation. Still room for improvements

--- testbench/modulo_tb.v
+++ testbench/modulo_tb.v
@@ -19,8 +19,8 @@ module modulo_tb #(parameter width = 32)();
     ***************************************************************************/
 
    modulo uut (
-      .in	(in),
-      .out  (out)
+      .in		(in),
+      .out  	(out)
    );
 
    /****************************************************************************
@@ -32,14 +32,35 @@ module modulo_tb #(parameter width = 32)();
       for (integer i=7; i < 65537; i = i * 3) begin
 			in = i;
 			#1;
-			$display ("%d mod 65537 = %d", in, out);
+			$display ("%b (%d) mod 65537 = %b (%d)", in, in, out, out);
 		end
 
-		#1 $display ("\nTesting value > 65537");
-		for (integer i=65546; i < 65636; i = i + 4) begin
+		#1 $display ("\nTesting value >= 65536");
+		for (integer i=65536; i < 65546; i = i + 1) begin
 			in = i;
 			#1;
-			$display ("%d mod 65537 = %d", in, out);
+			$display ("%b (%d) mod 65537 = %b (%d)", in, in, out, out);
+		end
+
+		#1 $display ("\nTesting value >= 131073");
+		for (integer i=131073; i < 131078; i = i + 1) begin
+			in = i;
+			#1;
+			$display ("%b (%d) mod 65537 = %b (%d)", in, in, out, out);
+		end
+
+		#1 $display ("\nTesting negative values > -65534");
+		for (integer i=0; i > -10; i = i - 1) begin
+			in = i;
+			#1;
+			$display ("%b (%d) mod 65537 = %b (%d)", i, i, out, out);
+		end
+
+		#1 $display ("\nTesting negative values from -65535");
+		for (integer i=-65535; i > -65546; i = i - 1) begin
+			in = i;
+			#1;
+			$display ("%b (%d) mod 65537 = %b (%d)", i, i, out, out);
 		end
 
       $finish;
