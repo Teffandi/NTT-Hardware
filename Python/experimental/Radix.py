c27f3ef2280131c32be281dc7b9c80e94d92e380
Teffandi <123167913+Teffandi@users.noreply.github.com>
2024-08-09 16:04:02 +0800

    Update Radix.py

--- Python/experimental/Radix.py
+++ Python/experimental/Radix.py
@@ -2,7 +2,7 @@ import math
 
 def Core_Radix_2(data_in,twiddle,mode): #behaviour of RTL model of R2
 
-    if (mode="NTT"):
+    if (mode=="NTT"):
         data_out[0]= data_in[0]+data_in[1]*twiddle
         data_out[1]= data_in[0]-data_in[1]*twiddle
         return data_out
@@ -17,7 +17,7 @@ def Core_Radix_4(data_in,twiddle,mode,w): #behaviour of RTL model of R4
 
     #calculating internal weights
     
-    if (mode="NTT"):
+    if (mode=="NTT"):
         data_in[0] = data_in[0]*twiddle[0]
         data_in[1] = data_in[1]*twiddle[1]
         data_in[2] = data_in[2]*twiddle[2]
@@ -48,7 +48,7 @@ def Core_Radix_8(data_in,twiddle,mode,w): #behaviour of RTL model of R8
 
     #calculating internal weights
     
-    if (mode="NTT"):
+    if (mode=="NTT"):
         data_in[0] = data_in[0]*twiddle[0]
         data_in[1] = data_in[1]*twiddle[1]
         data_in[2] = data_in[2]*twiddle[2]
